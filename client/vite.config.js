31fc9c9494d72fd811ebd4e33bf2f53468a955bd
Ritik Sharma <125951457+dev-ritiksharma553@users.noreply.github.com>
2025-08-27 01:12:23 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",  // 👈 this ensures routing works on deploy
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
