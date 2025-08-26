59b8cf3dd1a5042bf6751f9a05969f7ae52aceff
Ritik Sharma <125951457+dev-ritiksharma553@users.noreply.github.com>
2025-08-27 01:58:16 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",  // 👈 this ensures routing works on deploy
-  plugins: [react(), tailwindcss()],
+  base: "/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  publicDir: "public",
+  build: { outDir: "dist" },
+});
