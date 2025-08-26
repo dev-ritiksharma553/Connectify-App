a70fbefb2da68031520b13cff81dde6a05e43d96
Ritik Sharma <125951457+dev-ritiksharma553@users.noreply.github.com>
2025-08-26 23:40:18 +0530

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoDb = async ()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/Insta');
+        await mongoose.connect('mongodb+srv://ritik9896714965:k1U8YZ0rcdtcwZSV@cluster0.0ig178f.mongodb.net/');
         console.log('Db is Connected');
     } catch (error) {
         console.log(`error: ${error}`);
