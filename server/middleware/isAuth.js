6c336d079ff6b52c7dd3138756eab3913bec3473
Ritik Sharma <125951457+dev-ritiksharma553@users.noreply.github.com>
2025-08-26 23:39:45 +0530

    Update isAuth.js

--- server/middleware/isAuth.js
+++ server/middleware/isAuth.js
@@ -2,24 +2,17 @@
 const jwt = require('jsonwebtoken')
 
 const isAuth = async (req, res, next) => {
-    try {
-        const token = req.cookies?.token;
-        if (!token) {
-            return res.status(400).json({
-                message: "token not found"
-            })
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (!decoded) {
-            return res.status(400).json({
-                message: "token unauthorize"
-            })
-        }
-        req.id = decoded.id;
-        next()
-    } catch (error) {
-        console.log(error);
-    }
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Token not found" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.id = decoded.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 
 
 }
